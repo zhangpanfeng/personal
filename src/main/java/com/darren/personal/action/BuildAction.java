20017c3397d56e3572ca2f65b1ffd0c75420ce30
Zhang <Darren.Zhang@nielsen.com>
2016-12-16 18:24:42 +0800

    add script

--- src/main/java/com/darren/personal/action/BuildAction.java
+++ src/main/java/com/darren/personal/action/BuildAction.java
@@ -5,15 +5,17 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BuildAction {
     private static final Logger LOG = Logger.getLogger(BuildAction.class);
-    private static final String BASE_PATH = System.getProperty("user.dir") + File.separator + "src" + File.separator
-            + "main" + File.separator + "resources" + File.separator + "build.sh";
+    private static final String SCRIPT_PATH = "scriptPath";
 
     @RequestMapping(value = "/build.do")
     public String build() {
@@ -22,23 +24,28 @@ public class BuildAction {
     }
 
     @RequestMapping(value = "/execShell.do")
-    public void execShell() {
-        String command = "chmod 777 " + BASE_PATH;
+    public void execShell(HttpServletRequest request) {
+        String shellScript = request.getServletContext().getInitParameter(SCRIPT_PATH);
+        
         Process process = null;
         try {
-            BufferedReader re = new BufferedReader( new InputStreamReader(new FileInputStream(new File(BASE_PATH))));
+            File scriptFile = ResourceUtils.getFile(shellScript);  
+            String scriptPath = scriptFile.getAbsolutePath();
+            LOG.info("scriptPath = " + scriptPath);
+            String command = "chmod 777 " + scriptPath;
+            BufferedReader re = new BufferedReader( new InputStreamReader(new FileInputStream(scriptFile)));
             String li = null;
             while ((li = re.readLine()) != null){  
                 LOG.info(li);
             } 
             process = Runtime.getRuntime().exec(command);
             BufferedReader reader = new BufferedReader( new InputStreamReader(process.getInputStream()));
+            int result = process.waitFor();
+            LOG.info("result = " + result);
             String line = null;
             while ((line = reader.readLine()) != null){  
                 LOG.info(line);
             } 
-            int result = process.waitFor();
-            LOG.info("result = " + result);
             reader.close();
             re.close();
         } catch (Exception e) {
