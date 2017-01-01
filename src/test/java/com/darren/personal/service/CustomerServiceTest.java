368b0306f521d4b62107eb38fe2086b3ebc2643b
Darren-company <sf0902@163.com>
2017-01-01 12:54:27 +0800

    test

--- src/test/java/com/darren/personal/service/CustomerServiceTest.java
+++ src/test/java/com/darren/personal/service/CustomerServiceTest.java
@@ -1,26 +1,25 @@
 package com.darren.personal.service;
 
 import java.util.Date;
-import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.darren.personal.entity.Customer;
+
 //means it extends SpringJUnit4ClassRunner
-@RunWith(SpringJUnit4ClassRunner.class) 
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:applicationContext.xml" })
 public class CustomerServiceTest {
     private static final Logger LOG = Logger.getLogger(CustomerServiceTest.class);
-    @Resource  
+    @Resource
     private CustomerService customerService;
-    
+
     @Test
     public void testInsert() {
         Customer customer = new Customer();
@@ -30,39 +29,35 @@ public class CustomerServiceTest {
         customer.setGender("M");
         customer.setEmailState("N");
         customer.setComment("这是备注");
-//        int result = customerService.insert(customer);
-//        System.out.println(result);
-//        System.out.println(customer.getId());
-        Assert.assertTrue(true);
+        // int result = customerService.insert(customer);
+        // System.out.println(result);
+        // System.out.println(customer.getId());
     }
-    
+
     @Test
-    public void testSelectAll(){
-//      List<Customer> list = customerService.selectAll();
-//      for(Customer customer : list){
-//          System.out.println(customer);
-//      }
-        Assert.assertTrue(true);
+    public void testSelectAll() {
+        // List<Customer> list = customerService.selectAll();
+        // for(Customer customer : list){
+        // System.out.println(customer);
+        // }
     }
-    
+
     @Test
-    public void testSelectByParameter(){
+    public void testSelectByParameter() {
         Customer p = new Customer();
         p.setName("Darren Zhang");
         // List<Customer> list = customerService.selectByParameter(p);
         // for(Customer customer : list){
         // System.out.println(customer);
         // }
-      Assert.assertTrue(true);
     }
-    
+
     @Test
-    public void testUpdateByParameter(){
+    public void testUpdateByParameter() {
         Customer p = new Customer();
         p.setPhone("18321353610");
         p.setSendTime(new Date());
         p.setEmailState("Y");
-        //int result = customerService.updateByParameter(p);
-        Assert.assertTrue(true);
+        // int result = customerService.updateByParameter(p);
     }
 }
